0641f7f7898c0d1b740382efa40746422dc430b1
User <user@example.com>
2026-03-31 20:17:36 +0300

    corrected IBookService.cs

--- Services/Interfaces/IBookService.cs
+++ Services/Interfaces/IBookService.cs
@@ -7,8 +7,8 @@ public interface IBookService
     public Task<List<BookResponse>> GetAllBooksAsync();
     public Task<BookResponse?> GetBookByIdAsync(Guid bookId);
     public Task<BookResponse> CreateBookAsync(string title, string author, int yearOfPublication);
-    public Task<BookResponse> UpdateBookAsync(string title, string author, int yearOfPublication, Guid bookId);
-    public Task<BookResponse> DeleteBookAsync(Guid bookId);
+    public Task<BookResponse?> UpdateBookAsync(string title, string author, int yearOfPublication, Guid bookId);
+    public Task<bool> DeleteBookAsync(Guid bookId);
 
 
 }
